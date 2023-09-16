6378090a5029ba6501fbc681a1772d5a84ebdf6e
Zaeem-Shah3321 <zaeemshah58@gmail.com>
2023-09-16 14:10:43 +0500

    message

---
+++ Programming Day 2/Cow.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main (){
+	cout << " ------------------"<< endl;
+	cout << "   o     ^__^        "<< endl;
+	cout << "     o   (oo)\\______  "<< endl;
+	cout << "         (__)\\      )\\/\\ "<< endl;
+	cout << "             ||----w |   " << endl;
+	cout << "             ||     ||    ";
+	}   
