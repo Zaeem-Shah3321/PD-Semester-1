fc73b7c0d299a9a77b1a5cbde16015b456504de9
Zaeem-Shah3321 <zaeemshah58@gmail.com>
2023-09-22 17:15:46 +0500

    message

---
+++ Programming Day 3/Task13(CP).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+main (){
+	int num;
+	int sum;
+	cout << "Enter 1st number: ";
+	cin >> num;
+	sum = sum + num;
+	cout << "Enter 2nd number: ";
+	cin >> sum;
+	sum = sum + num;
+	cout << "Enter 3rd number: ";
+	cin >> num;
+	sum = sum + num;
+	cout << "Enter 4th number: ";
+	cin >> num;
+	sum = sum + num;
+	cout << "Enter 5th number: ";
+	cin >> num;
+	sum = sum + num;
+	cout << "Sum of 5 digits is: " << sum ;
+}
